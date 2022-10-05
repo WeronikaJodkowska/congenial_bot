d13c95851d3f0a5c3cac165caddc3a24329296c3
Weronika Jodkowska <veronica.jodkowska@gmail.com>
2022-10-05 19:30:22 +0300

    Lesson 36 AioHTTP pt.2

---
+++ schemas.py
@@ -0,0 +1,10 @@
+from marshmallow import Schema, fields
+
+
+class MessageSchema(Schema):
+    chat_id = fields.Int()
+    message = fields.Str(required=True)
+
+
+
+
--- services.py
+++ services.py
@@ -1,16 +1,27 @@
 import os
+import random
 
 import aiohttp
+import aioredis
 from telegram import Update, Bot
 from telegram.ext import ContextTypes
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
+REDIS_URL = os.getenv("REDIS")
+redis = aioredis.from_url(REDIS_URL)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f"Start {update.effective_user.first_name}")
+    await redis.lpush("chats", update.effective_chat.id)
+
+    chat_ids = []
+    chat_len = await redis.llen("chats")
+    for index in range(chat_len):
+        chat_id = await redis.lindex("chats", index)
+        chat_ids.append(chat_id.decode())
+    await update.message.reply_text(f", ".join(chat_ids))
 
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -18,9 +29,16 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def top_products(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await get_products("http://172.18.0.1:8000/api/products/?ordering=-cost", update, context)
+
+
+async def popular_products(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await get_products("http://172.18.0.1:8000/api/products/popular", update, context)
+
+
+async def get_products(url: str, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     async with aiohttp.ClientSession(trust_env=True) as session:
-        # async with session.get("http://127.0.0.1:8000/api/products/?ordering=-cost") as response:
-        async with session.get("http://172.18.0.1:8000/api/products/?ordering=-cost") as response:
+        async with session.get(url) as response:
             products = await response.json()
     result = ""
     for product in products["results"]:
